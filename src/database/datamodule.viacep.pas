819cb5d07cba3dec94241116fadcd0cf61583acc
Anderson Gaitolini <gaitolini@gmail.com>
2024-09-23 14:48:24 -0300

    Adicionado um arquivo ini para configuração do DB

--- src/database/datamodule.viacep.pas
+++ src/database/datamodule.viacep.pas
@@ -41,17 +41,36 @@ implementation
 
 { Tdm }
 
-procedure Tdm.InitializeDatabase;
-begin
+uses
+  IniFiles, System.IOUtils;
 
-  conViacep.Disconnect;
-  conViacep.ProviderName := 'PostgreSQL';
-  conViacep.Server := 'localhost';
-  conViacep.Database := 'viacepdb';
-  conViacep.Username := 'postgres';
-  conViacep.Password := '04060811';
-  conViacep.Port := 5432;
-  conViacep.Connect;
+{ Tdm }
+
+procedure Tdm.InitializeDatabase;
+var
+  IniFile: TIniFile;
+  IniFilePath: string;
+begin
+  IniFilePath := TPath.Combine(ExtractFilePath(ParamStr(0)), 'config.ini');
+
+  if not FileExists(IniFilePath) then
+    raise Exception.Create('Arquivo de configuração "config.ini" não encontrado!');
+
+  IniFile := TIniFile.Create(IniFilePath);
+  try
+    conViacep.Disconnect;
+
+    conViacep.ProviderName := 'PostgreSQL';
+    conViacep.Server := IniFile.ReadString('Database', 'Server', 'localhost');
+    conViacep.Database := IniFile.ReadString('Database', 'Database', 'viacepdb');
+    conViacep.Username := IniFile.ReadString('Database', 'Username', 'postgres');
+    conViacep.Password := IniFile.ReadString('Database', 'Password', '');
+    conViacep.Port := IniFile.ReadInteger('Database', 'Port', 5432);
+
+    conViacep.Connect;
+  finally
+    IniFile.Free;
+  end;
 end;
 
 end.
